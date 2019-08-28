aad03f02d1250b501b8b4bbf2aa3f54c3d71316a
Shadowmaple <1142319190@qq.com>
2019-08-28 09:53:33 +0800

    better modification

--- sql/176_Second_Highest_Salary.sql
+++ sql/176_Second_Highest_Salary.sql
@@ -1,5 +1,4 @@
-select ifnull((select distinct Salary 
-            from Employee order by Salary DESC limit 1,1),
-			null) as SecondHighestSalary
-    from Employee
-    limit 1;
+select ifnull((select distinct Salary	
+		from Employee
+		order by Salary DESC 
+		limit 1,1), null) as SecondHighestSalary;
