ff2edbd377b49a6e5394af2d3140dc1a5bb158ff
Shadowmaple <1142319190@qq.com>
2019-08-28 10:50:39 +0800

    add better solution

--- sql/181_Employees_Earning_More_Than_Their_Managers.sql
+++ sql/181_Employees_Earning_More_Than_Their_Managers.sql
@@ -1,5 +1,17 @@
+-- 854ms，子查询
 select e.Name as Employee
     from Employee as e
     where e.Salary > (select m.Salary from Employee as m
 					where m.Id = e.ManagerId)
     ;
+
+
+-- 更优的题解，使用表联结，506ms
+/*
+SELECT
+     a.NAME AS Employee
+FROM Employee AS a JOIN Employee AS b
+     ON a.ManagerId = b.Id
+     AND a.Salary > b.Salary
+;
+*/
