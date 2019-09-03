5cbd52d7072e0085a9c6801e3e3f9710de452c35
Shadowmaple <1142319190@qq.com>
2019-09-03 11:04:32 +0800

    new

---
+++ go/9_Palindrome_Number.go
@@ -0,0 +1,17 @@
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    } else if x == 0 {
+        return true
+    }
+
+    a, b := 0, x
+    for b != 0 {
+        a = a * 10 + b % 10
+        b /= 10
+    }
+    if a == x {
+        return true
+    }
+    return false
+}
