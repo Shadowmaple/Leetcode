bbf69980dea91bb10b96046fcc4a763948502071
Shadowmaple <1142319190@qq.com>
2019-09-03 10:31:02 +0800

    modify with a better solution

---
+++ go/1_Two_Sum.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+    numMap := make(map[int]int)
+    for index, num := range(nums) {
+        m := target - num
+        if i, exist := numMap[m]; exist {
+            return []int{i, index}
+        }
+        numMap[num] = index
+    }
+    return nil
+}
