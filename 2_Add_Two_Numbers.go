b49b49562200976525dcc063cb5bc297c5256377
Shadowmaple <1142319190@qq.com>
2019-09-06 20:30:50 +0800

    modify, better method, less memory consumption

--- 2_Add_Two_Numbers.go
+++ 2_Add_Two_Numbers.go
@@ -6,52 +6,32 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    head := new(ListNode)
+    head := &ListNode{Next: l2}
     tail := head
-    pre := tail
     flag := 0
-
-    for l1 != nil && l2 != nil {
-        sum := l1.Val + l2.Val + flag
-        tail.Val = sum % 10
+    
+    for ; l1 != nil; l1 = l1.Next {
+        if tail.Next == nil {
+            tail.Next = &ListNode{}
+        }
+        tail = tail.Next
+        sum := l1.Val + tail.Val + flag
         flag = sum / 10
-        l1 = l1.Next
-
-        l2 = l2.Next
-
-        pre, tail = next(pre, tail)
+        tail.Val = sum % 10
     }
-    if l1 != nil || l2 != nil {
-        for l1 != nil {
-            sum := l1.Val + flag
-            tail.Val = sum % 10
-            flag = sum / 10
-
-            l1 = l1.Next
-            pre, tail = next(pre, tail)
-        }
-        for l2 != nil {
-            sum := l2.Val + flag
-            tail.Val = sum % 10
-            flag = sum / 10
-
-            l2 = l2.Next
-            pre, tail = next(pre, tail)
-        }
+    for tail.Next != nil {
+        tail = tail.Next
+        sum := tail.Val + flag
+        flag = sum / 10
+        tail.Val = sum % 10
     }
+    
     if flag > 0 {
-        tail.Val = flag
-        pre, tail = next(pre, tail)
+        if tail.Next == nil {
+            tail.Next = &ListNode{}
+        }
+        tail.Next.Val = flag
     }
-    pre.Next = nil
-    return head
+    
+    return head.Next
 }
-
-func next(pre, tail *ListNode) (*ListNode, *ListNode){
-    pre = tail
-    p := new(ListNode)
-    tail.Next = p
-    tail = p
-    return pre, tail
-}
-
