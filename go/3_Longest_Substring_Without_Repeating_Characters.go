e095fafe4ff0a0873e50dffa2898f9d6ef918e34
Shadowmaple <1142319190@qq.com>
2019-09-07 10:08:44 +0800

    better solution

--- go/3_Longest_Substring_Without_Repeating_Characters.go
+++ go/3_Longest_Substring_Without_Repeating_Characters.go
@@ -1,32 +1,23 @@
-// 傻瓜方法，遍历子串，从最长的开始通不过，要从最短的开始
 func lengthOfLongestSubstring(s string) int {
-    l := len(s)
-    count := 0
-    for size := 1; size <= l; size++ {
-        flag := false
-        for i := 0; i + size <= l; i++ {
-            c := s[i : i + size]
-            if !isRepeat(c) {
-                count++
-                flag = true
-                break
-            }
-        }
-        if !flag {
-            return count
-        }
-    }
-    return count
-}
-
-func isRepeat(s string) bool {
+    max := 0
+    l := make([]byte, 0)
+    var c string
     for i := 0; i < len(s); i++ {
-        c := []byte(s)
-        a := append(c[:i], c[i+1:]...)
-        if strings.Contains(string(a), string(s[i])) {
-            return true
+        j := strings.Index(c, string(s[i]))
+        if j == -1 {
+            l = append(l, s[i])
+            c = string(l)
+            if lens := len(l); max < lens {
+                max = lens
+            }
+            continue
+        }
+        l = append(l[j + 1:], l[j])
+        c = string(l)
+        if lens := len(l); max < lens {
+            max = lens
         }
     }
-    return false
+    return max
 }
 
