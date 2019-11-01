a7a6718fffd54b797b4b7290061bb3afbbf69642
Shadowmaple <1142319190@qq.com>
2019-11-01 08:51:29 +0800

    new

--- c++/234_Palindrome_Linked_List.cpp
+++ c++/234_Palindrome_Linked_List.cpp
@@ -1,4 +1,5 @@
 # include <iostream>
+# include <stack>
 using namespace std;
 
 // Definition for singly-linked list.
@@ -11,6 +12,25 @@ struct ListNode {
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return true;
 
+        stack <int>stk;
+        ListNode *low = head, *fast = head;
+        stk.push(low->val);
+        while (fast->next && fast->next->next) {
+            fast = fast->next->next;
+            low = low->next;
+            stk.push(low->val);
+        }
+        if (fast->next == nullptr)
+            stk.pop();
+        while (low->next) {
+            low = low->next;
+            if (!stk.empty() && stk.top() != low->val)
+                return false;
+            stk.pop();
+        }
+        return true;
     }
 };
