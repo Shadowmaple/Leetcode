9b0cc441a48d9bcec1736c9937de91aed1af4c42
Shadowmaple <1142319190@qq.com>
2020-02-01 12:40:25 +0800

    new

---
+++ c++/71_Simplify_Path.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+# include <string>
+# include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> sk;
+        string s;
+        int len = path.length();
+        for (int i = 0; i < len; i++) {
+            if (path[i] != '/') {
+                s += path[i];
+                if (i != len-1) continue;
+            }
+            if (s == "") continue;
+            if (s == ".." && !sk.empty())
+                sk.pop_back();
+            else if (s != ".." && s != ".")
+                sk.push_back(s);
+
+            s.clear();
+        }
+        if (sk.empty()) return "/";
+        for (auto iter=sk.begin(); iter != sk.end(); iter++)
+            s += "/" + *iter;
+
+        return s;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.simplifyPath("/a/../../b/../c//.//") << endl;
+    return 0;
+}
