d37ea43098119feb4f2e10c4aeff31fc1960c3bb
Shadowmaple <1142319190@qq.com>
2020-02-05 14:12:10 +0800

    better

--- c++/46_Permutations.cpp
+++ c++/46_Permutations.cpp
@@ -4,27 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> v;
-    int len;
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> cmb;
-        len = nums.size();
-        backTracing(nums, cmb);
+        vector<vector<int>> v;
+        backTracing(v, nums, nums.size(), 0);
         return v;
     }
 
-    void backTracing(vector<int>& nums, vector<int>& cmb) {
-        if (cmb.size() == len) {
-            v.push_back(cmb);
+    void backTracing(vector<vector<int>>& v, vector<int>& nums, int len, int idx) {
+        if (idx == len) {
+            v.push_back(nums);
             return ;
         }
 
-        for (int idx = 0; idx < len; idx++) {
-            if (nums[idx] == ' ') continue;
-            vector<int> x = cmb, y = nums;
-            x.push_back(nums[idx]);
-            y[idx] = ' ';
-            backTracing(y, x);
+        for (int i = idx; i < len; i++) {
+            swap(nums[i], nums[idx]);
+            backTracing(v, nums, len, idx+1);
+            swap(nums[i], nums[idx]);
         }
     }
 };
