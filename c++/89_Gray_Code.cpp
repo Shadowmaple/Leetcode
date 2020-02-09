91c638fa7d050286204d3a48dd2eb4e42f098aea
Shadowmaple <1142319190@qq.com>
2020-02-09 12:02:00 +0800

    better

--- c++/89_Gray_Code.cpp
+++ c++/89_Gray_Code.cpp
@@ -32,3 +32,15 @@ public:
         backTracking(s, n, idx+1);
     }
 };
+
+// 公式简单推导
+// 格雷码生成公式：G(i) = i ^ (i/2)
+class Solution2 {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> v;
+        for (int i = 0; i < 1 << n; i++)
+            v.push_back(i ^ (i >> 1));
+        return v;
+    }
+};
