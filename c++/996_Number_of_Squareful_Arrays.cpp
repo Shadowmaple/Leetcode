3d891d81c2d66ae045c119e5d66dd7047c6f250b
Shadowmaple <1142319190@qq.com>
2020-02-20 15:32:03 +0800

    AC

--- c++/996_Number_of_Squareful_Arrays.cpp
+++ c++/996_Number_of_Squareful_Arrays.cpp
@@ -10,27 +10,23 @@ public:
         sort(A.begin(), A.end());
         vector<bool> visited(A.size(), false);
         vector<int> cmb;
-        return DFS(A, cmb, visited);
+        return DFS(A, visited, 0, 0);
     }
 
-    int DFS(vector<int>& A, vector<int>& cmb, vector<bool>& visited) {
-        if (cmb.size() == A.size()) {
-            for (int i = 0; i < cmb.size()-1; i++) {
-                int n = cmb[i] + cmb[i+1];
-                int x = sqrt(n);
-                if (pow(x, 2) != n) return 0;
-            }
+    int DFS(vector<int>& A, vector<bool>& visited, int pre, int idx) {
+        if (idx == A.size())
             return 1;
-        }
 
         int count = 0;
         for (int i = 0; i < A.size(); i++) {
             if (visited[i] || i > 0 && A[i] == A[i-1] && !visited[i-1])
                 continue;
+            if (idx) {
+                int n = pre + A[i], x = sqrt(n);
+                if (pow(x, 2) != n) continue;
+            }
             visited[i] = true;
-            cmb.push_back(A[i]);
-            count += DFS(A, cmb, visited);
-            cmb.pop_back();
+            count += DFS(A, visited, A[i], idx+1);
             visited[i] = false;
         }
         return count;
