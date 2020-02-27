021051379df3921ff8cdc4ea26e3e7b113df2eac
Shadowmaple <1142319190@qq.com>
2020-02-27 12:41:35 +0800

    better

--- c++/217_Contains_Duplicate.cpp
+++ c++/217_Contains_Duplicate.cpp
@@ -1,8 +1,11 @@
 # include <iostream>
 # include <vector>
 # include <map>
+# include <set>
+# include <algorithm>
 using namespace std;
 
+// map
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -12,6 +15,26 @@ public:
             if (!visited[n]) return true;
             visited[n] = true;
         }
+        return false;
+    }
+};
+
+// 集合，比较数量
+class Solution2 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s(nums.begin(), nums.end());
+        return s.size() != nums.size();
+    }
+};
+
+// 排序，相邻是否相同
+class Solution3 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++)
+            if (nums[i] == nums[i-1]) return true;
         return false;
     }
 };
