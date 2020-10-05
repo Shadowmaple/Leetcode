a96b44cb6408b24805a5914fc98727f20eda7344
Shadowmaple <1142319190@qq.com>
2020-10-05 15:29:54 +0800

    better solution

--- 101_Symmetric_Tree.cpp
+++ 101_Symmetric_Tree.cpp
@@ -49,3 +49,19 @@ public:
         return true;
     }
 };
+
+// 递归
+class Solution2 {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return check(root->left, root->right);
+    }
+
+    bool check(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+
+        return p->val == q->val && check(p->left, q->right) && check(p->right, q->left);
+    }
+};
