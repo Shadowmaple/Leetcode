d7abf5675be96d631ebc18a7c266d4575f4c1a92
Shadowmaple <1142319190@qq.com>
2020-11-26 20:52:48 +0800

    second solution--topologicalSort

--- 329_Longest_Increasing_Path_in_a_Matrix.cpp
+++ 329_Longest_Increasing_Path_in_a_Matrix.cpp
@@ -41,3 +41,52 @@ public:
         return cp[x][y];
     }
 };
+
+// 拓扑排序
+class Solution2 {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if (matrix.empty()) return 0;
+
+        int m = matrix.size(), n = matrix[0].size();
+        // 入度数
+        vector<vector<int>> inNum = vector<vector<int>>(m, vector<int>(n));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int cur = matrix[i][j];
+                if (i && cur < matrix[i-1][j])       inNum[i-1][j]++;
+                if (j && cur < matrix[i][j-1])       inNum[i][j-1]++;
+                if (i < m-1 && cur < matrix[i+1][j]) inNum[i+1][j]++;
+                if (j < n-1 && cur < matrix[i][j+1]) inNum[i][j+1]++;
+            }
+        }
+
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                if (!inNum[i][j]) q.push({i, j});
+        }
+
+        int level = 0;
+        while (!q.empty()) {
+            level++;
+            for (int i = q.size(); i > 0; i--) {
+                auto top = q.front();
+                q.pop();
+
+                int x = top.first, y = top.second;
+                printf("%d-%d-%d ", x, y, level);
+                int cur = matrix[x][y];
+                if (x && cur < matrix[x-1][y])
+                    if (--inNum[x-1][y] == 0) q.push({x-1, y});
+                if (x < m && cur < matrix[x+1][y])
+                    if (--inNum[x+1][y] == 0) q.push({x+1, y});
+                if (y && cur < matrix[x][y-1])
+                    if (--inNum[x][y-1] == 0) q.push({x, y-1});
+                if (y < n && cur < matrix[x][y+1])
+                    if (--inNum[x][y+1] == 0) q.push({x, y+1});
+            }
+        }
+        return level;
+    }
+};
