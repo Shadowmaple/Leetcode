4c0ea9043157e5e2555029873c706e9de7e664d3
Shadowmaple <1142319190@qq.com>
2020-12-04 16:48:57 +0800

    update, use single set

--- 752_OpenTheLock.cpp
+++ 752_OpenTheLock.cpp
@@ -7,10 +7,12 @@ using namespace std;
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
+        if (target == "0000") return 0;
         queue<string> q;
-        set<string> st, dead;
+        set<string> st; // 保存已访问过的节点
         int path = 0;
-        for (auto s : deadends) dead.insert(s);
+        // 直接将 deadends 加入 st 中，就不用另外一个 set 了
+        for (auto s : deadends) st.insert(s);
 
         if (!st.count("0000")) q.push("0000");
         while (!q.empty()) {
@@ -25,11 +27,11 @@ public:
                     s1[i] = s1[i] < '9'? s1[i] + 1:'0';
                     s2[i] = s2[i] > '0'? s2[i] - 1:'9';
 
-                    if (!st.count(s1) && !dead.count(s1)) {
+                    if (!st.count(s1)) {
                         st.insert(s1);
                         q.push(s1);
                     }
-                    if (!st.count(s2) && !dead.count(s2)) {
+                    if (!st.count(s2)) {
                         st.insert(s2);
                         q.push(s2);
                     }
