28d0458569e554e5cc3bc6ab90527b0ea528290f
Shadowmaple <1142319190@qq.com>
2020-12-30 10:47:52 +0800

    better solution--dp

--- target_offer/46.cpp
+++ target_offer/46.cpp
@@ -2,6 +2,7 @@
 # include <string>
 using namespace std;
 
+// DFS
 class Solution {
 public:
     int translateNum(int num) {
@@ -16,5 +17,21 @@ public:
         if (s[idx] != '0' && idx < s.length()-1 && stoi(s.substr(idx, 2)) < 26)
             num += DFS(s, idx+2);
         return num;
+    }
+};
+
+// 动态规划
+class Solution {
+public:
+    int translateNum(int num) {
+        string s = to_string(num);
+        int dp[s.length()+1];
+        dp[0] = dp[1] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i-1] != '0' && stoi(s.substr(i-1, 2)) < 26)
+                dp[i+1] = dp[i] + dp[i-1];
+            else dp[i+1] = dp[i];
+        }
+        return dp[s.length()];
     }
 };
