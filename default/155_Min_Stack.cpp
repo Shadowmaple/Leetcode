37dad6f82eb898ab4257a4a0622b02ad70c56a2b
Shadowmaple <1142319190@qq.com>
2021-03-31 11:53:36 +0800

    update

--- default/155_Min_Stack.cpp
+++ default/155_Min_Stack.cpp
@@ -34,11 +34,51 @@ private:
     stack<int> minStk;
 };
 
+
+// 
+class MinStack2 {
+public:
+    stack<long> sk;
+    int minVal;
+    /** initialize your data structure here. */
+    MinStack() {
+        minVal = 0;
+    }
+    
+    void push(int val) {
+        if (sk.empty()) {
+            sk.push(0);
+            minVal = val;
+            return ;
+        }
+        long diff = long(val) - long(minVal);
+        if (diff < 0) minVal = val;
+        sk.push(diff);
+    }
+    
+    void pop() {
+        if (sk.top() < 0) {
+            minVal = minVal - sk.top();
+        }
+        sk.pop();
+    }
+    
+    int top() {
+        if (sk.top() < 0) return minVal;
+        return sk.top() + minVal;
+    }
+    
+    int getMin() {
+        return minVal; 
+    }
+};
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(x);
+ * obj->push(val);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
