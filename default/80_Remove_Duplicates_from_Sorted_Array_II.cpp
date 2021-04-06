b47580e63a6c9c82ee40c12c852efcd16c6bd18a
Shadowmaple <1142319190@qq.com>
2021-04-06 11:42:50 +0800

    :fire: new solution

--- default/80_Remove_Duplicates_from_Sorted_Array_II.cpp
+++ default/80_Remove_Duplicates_from_Sorted_Array_II.cpp
@@ -13,3 +13,24 @@ public:
         return nums.size();
     }
 };
+
+// 原地删除，双指针
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l = 1, r = 1;
+        int cur = nums[0], count = 1;
+        while (r < nums.size()) {
+            if (nums[r] != cur) {
+                cur = nums[r];
+                nums[l++] = nums[r];
+                count = 1;
+            } else if (count < 2) {
+                nums[l++] = nums[r];
+                count++;
+            }
+            r++;
+        }
+        return l;
+    }
+};
