a86685038d81c61a7b9317fdefd959f38d55f81e
Shadowmaple <1142319190@qq.com>
2021-04-10 11:18:49 +0800

    new

---
+++ default/48.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int limit = n-2;
+        for (int i = 0; i < n/2; i++) {
+            int temp;
+            for (int j = i; j <= limit; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
+            }
+            limit--;
+        }
+    }
+};
